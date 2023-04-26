5e2d39b906d4ea4d77fccc6b2a84400ac5651117
smedina1304 <smedina1304@gmail.com>
2023-04-26 17:22:29 -0300

    Lab 1 - Testes

---
+++ lab01/cmd_docker_mysql.sh
@@ -0,0 +1,1 @@
+docker exec -it  db-mysql mysql -h mysql-db -P 3306  -u root -p123
--- lab01/sql_query_data.sql
+++ lab01/sql_query_data.sql
@@ -1,2 +1,27 @@
 select recvTime, attrName, attrType, attrValue
 from `urn_ngsi-ld_Server_001_Computer`;
+
+
+SELECT
+    UNIX_TIMESTAMP(recvTime) as "time",
+    attrName as "name",
+    CAST(attrValue as decimal) as "valor"
+FROM `urn_ngsi-ld_Server_001_Computer`
+ORDER BY "time"
+
+
+SELECT
+    UNIX_TIMESTAMP(recvTime) as "time",
+    attrName as "name",
+    CAST(attrValue as decimal) as "valor"
+FROM `urn_ngsi-ld_Server_001_Computer`
+WHERE attrName = "cpu"
+ORDER BY "time" 
+
+SELECT
+    UNIX_TIMESTAMP(recvTime) as "time",
+    attrName as "name",
+    CAST(attrValue as decimal) as "valor"
+FROM `urn_ngsi-ld_Server_001_Computer`
+WHERE attrName = "memory"
+ORDER BY "time" 
