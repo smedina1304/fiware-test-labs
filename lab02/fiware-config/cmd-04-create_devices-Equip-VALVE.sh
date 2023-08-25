5deec7d24a6ef8b4c317e082cb56675e7a850b25
smedina1304 <smedina1304@gmail.com>
2023-08-25 20:20:34 -0300

    Lab 02 - Configurado - iotAgenteJSON

--- lab02/fiware-config/cmd-04-create_devices-Equip-VALVE.sh
+++ lab02/fiware-config/cmd-04-create_devices-Equip-VALVE.sh
@@ -2,4 +2,4 @@ curl --location 'http://localhost:4041/iot/devices' \
 --header 'Content-Type: application/json' \
 --header 'fiware-service: poyry' \
 --header 'fiware-servicepath: /' \
---d @config_devices-Equip-COOLER.json
+-d @config_devices-Equip-VALVE.json
