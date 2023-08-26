4f5d9e0c84b70e4c9e04fec982f55012d500156f
smedina1304 <smedina1304@gmail.com>
2023-08-26 17:29:28 -0300

    Lab 02 - Configurando Notificação Cygnus - PUMPS

--- lab02/fiware-config/cmd-09-create_cygnus_notification_PUMP.sh
+++ lab02/fiware-config/cmd-09-create_cygnus_notification_PUMP.sh
@@ -3,11 +3,65 @@ curl --location 'http://localhost:1026/v2/subscriptions/' \
 --header 'fiware-service: poyry' \
 --header 'fiware-servicepath: /' \
 --data '{
-    "description": "Notify Cygnus of Pumps changes",
+    "description": "Notify Cygnus - PUMP1",
     "subject": {
         "entities": [
             {
-                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:PUMP*",
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:PUMP1",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "OPERATION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - PUMP2",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:PUMP2",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "OPERATION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - PUMP3",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:PUMP3",
                 "type": "Equip"
             }
         ]
