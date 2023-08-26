2a8876aeef8d9a4cb56497e9e7915101c1a8d775
smedina1304 <smedina1304@gmail.com>
2023-08-26 17:29:11 -0300

    Lab 02 - Configurando Notificação Cygnus - VALVES

--- lab02/fiware-config/cmd-08-create_cygnus_notification_VALVE.sh
+++ lab02/fiware-config/cmd-08-create_cygnus_notification_VALVE.sh
@@ -3,11 +3,174 @@ curl --location 'http://localhost:1026/v2/subscriptions/' \
 --header 'fiware-service: poyry' \
 --header 'fiware-servicepath: /' \
 --data '{
-    "description": "Notify Cygnus of Valves changes",
+    "description": "Notify Cygnus - VALVE_1_IN",
     "subject": {
         "entities": [
             {
-                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE*",
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_1_IN",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "POSITION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - VALVE_1_OUT",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_1_OUT",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "POSITION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - VALVE_1_RET",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_1_RET",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "POSITION"
+        ]
+    },
+    "throttling": 5
+}'
+
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - VALVE_2_IN",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_2_IN",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "POSITION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - VALVE_2_OUT",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_2_OUT",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "POSITION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - VALVE_2_RET",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_2_RET",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "POSITION"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - VALVE_3_RET",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:VALVE_3_RET",
                 "type": "Equip"
             }
         ]
