97a240300201c3cf4c7c2a1040293cc6fbd5ad08
smedina1304 <smedina1304@gmail.com>
2023-08-26 17:29:41 -0300

    Lab 02 - Configurando Notificação Cygnus - COOLERS

--- lab02/fiware-config/cmd-10-create_cygnus_notification_COOLER.sh
+++ lab02/fiware-config/cmd-10-create_cygnus_notification_COOLER.sh
@@ -3,11 +3,38 @@ curl --location 'http://localhost:1026/v2/subscriptions/' \
 --header 'fiware-service: poyry' \
 --header 'fiware-servicepath: /' \
 --data '{
-    "description": "Notify Cygnus of Coolers changes",
+    "description": "Notify Cygnus - COOLER1",
     "subject": {
         "entities": [
             {
-                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:COOLER*",
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:COOLER1",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "TEMPERATURE"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - COOLER2",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:COOLER2",
                 "type": "Equip"
             }
         ]
