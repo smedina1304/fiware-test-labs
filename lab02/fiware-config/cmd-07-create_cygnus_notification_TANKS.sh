e17d7c2732699b458619eb434f6ea906e16b8fdd
smedina1304 <smedina1304@gmail.com>
2023-08-26 16:58:27 -0300

    Lab 02 - Configurando Notificação Cygnus - TANKS

--- lab02/fiware-config/cmd-07-create_cygnus_notification_TANKS.sh
+++ lab02/fiware-config/cmd-07-create_cygnus_notification_TANKS.sh
@@ -3,11 +3,87 @@ curl --location 'http://localhost:1026/v2/subscriptions/' \
 --header 'fiware-service: poyry' \
 --header 'fiware-servicepath: /' \
 --data '{
-    "description": "Notify Cygnus of Tanks changes",
+    "description": "Notify Cygnus - Envase01:TQ1",
     "subject": {
         "entities": [
             {
-                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:TQ*",
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:TQ1",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "LEVEL",
+            "LEVEL_HIGH",
+            "LEVEL_HIGH_ALARM",
+            "LEVEL_LOW",
+            "LEVEL_LOW_ALARM",
+            "VOLUME",
+            "CAPACITY",
+            "CAPACITY_UNIT",
+            "TEMPERATURE",
+            "TEMP_ALARM",
+            "TEMP_LOW",
+            "TEMP_HIGH"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - Envase01:TQ2",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:TQ2",
+                "type": "Equip"
+            }
+        ]
+    },
+    "notification": {
+        "http": {
+            "url": "http://cygnus:5050/notify"
+        },
+        "attrs": [
+            "STATUS_ID",
+            "STATUS_DESC",
+            "LEVEL",
+            "LEVEL_HIGH",
+            "LEVEL_HIGH_ALARM",
+            "LEVEL_LOW",
+            "LEVEL_LOW_ALARM",
+            "VOLUME",
+            "CAPACITY",
+            "CAPACITY_UNIT",
+            "TEMPERATURE",
+            "TEMP_ALARM",
+            "TEMP_LOW",
+            "TEMP_HIGH"
+        ]
+    },
+    "throttling": 5
+}'
+
+curl --location 'http://localhost:1026/v2/subscriptions/' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: poyry' \
+--header 'fiware-servicepath: /' \
+--data '{
+    "description": "Notify Cygnus - Envase01:TQ3",
+    "subject": {
+        "entities": [
+            {
+                "idPattern": "urn:ngsi-ld:SiteSP:Envase01:TQ3",
                 "type": "Equip"
             }
         ]
